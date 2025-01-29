aff505311424598c5e257e848d82f940e8fe8537
Alena Nish <nkm.nnov@gmail.com>
2025-01-29 11:02:45 +0300

    Add prime/base colors and L size

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,7 +1,7 @@
 import React, { ButtonHTMLAttributes, MouseEvent, ReactNode } from "react";
 
 type ButtonType = "primary" | "secondary" | "tertiary";
-type ButtonSize = "sm" | "md" | "lg";
+type ButtonSize = "s" | "m" | "l";
 
 type ButtonState = "enabled" | "pressed" | "hover" | "disabled";
 
@@ -9,6 +9,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   type?: ButtonType;
   size?: ButtonSize;
   state?: ButtonState;
+  color?: "prime" | "base";
   icon?: ReactNode;
   iconPosition?: "left" | "right";
   children?: ReactNode;
@@ -18,8 +19,9 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Button: React.FC<ButtonProps> = ({
   type = "primary",
-  size = "md",
+  size = "m",
   state = "enabled",
+  color = 'prime',
   icon,
   iconPosition = "left",
   children,
@@ -28,50 +30,79 @@ const Button: React.FC<ButtonProps> = ({
   className = "",
   ...rest
 }) => {
-  const baseStyles =
-    "font-medium flex flex-row items-center gap-2  rounded-[32px] ";
+  const baseStyles = "font-medium flex flex-row items-center justify-center gap-2  rounded-[32px] ";
 
   const typeStyles = () => {
     switch (type) {
       case "primary":
-        return `bg-blue-500 
-          text-white-0 
-          hover:bg-blue-600 
-          focus:bg-blue-700 
-          disabled:bg-white-200
-          disabled:hover:bg-blue-300`;
+        if (color === 'prime') {
+          return `bg-prime-500 
+            text-base-0 
+            hover:bg-prime-600 
+            focus:bg-prime-700 
+            disabled:bg-base-200`;
+          } else {
+            return `bg-base-500 
+            text-base-0 
+            hover:bg-base-700 
+            focus:bg-base-900 
+            disabled:bg-base-200`;
+          }
       case "secondary":
-        return `text-blue-500
-          border
-          border-blue-500
-          hover:bg-blue-50 
-          focus:border-blue-700
-          focus:text-blue-700
-          disabled:border-white-200
-          disabled:text-white-200 
-          disabled:hover:bg-white-100`;
+        if (color === 'prime') {
+          return `text-prime-500
+            border-2
+            border-prime-500
+            hover:text-prime-600
+            hover:border-prime-600
+            focus:border-prime-700
+            focus:text-prime-700
+            disabled:border-base-200
+            disabled:text-base-200 `;
+          } else {
+            return `text-base-500
+            border-2
+            border-base-500
+            hover:text-base-800
+            hover:border-base-800
+            focus:border-base-900
+            focus:text-base-900
+            disabled:border-base-200
+            disabled:text-base-200 `;
+          }
       case "tertiary":
-        return `text-blue-500 
-          hover:text-blue-600 
-          focus:bg-blue-50
-          focus:text-blue-700
-          disabled:text-white-200 
-          disabled:hover:text-blue-300`;
+        if (color === 'prime') {
+        return `text-prime-500 
+          hover:text-prime-600
+          hover:bg-prime-50 
+          focus:bg-prime-100
+          focus:text-prime-700
+          disabled:text-base-200`;
+        } else {
+          return `text-base-700 
+          hover:text-base-800
+          hover:bg-base-50 
+          focus:bg-base-100
+          focus:text-base-900
+          disabled:text-base-200`;
+        }
       default:
-        return `bg-blue-500 
-          text-white 
-          hover:bg-blue-600 
-          disabled:bg-blue-300 
-          disabled:hover:bg-blue-300`;
+        return `bg-prime-500 
+          text-base-0 
+          hover:bg-prime-600 
+          focus:bg-prime-700 
+          disabled:bg-base-200`;
     }
   };
 
   const sizeStyles = () => {
     switch (size) {
-      case "sm":
+      case "s":
         return "h-10 text-body-s px-4 py-2 ";
-      case "md":
+      case "m":
         return "h-[48px] text-body-m px-6 py-1 ";
+      case "l":
+        return "h-[70px] text-h4 px-6 py-1 ";
       default:
         return "px-4 py-2 text-body-m ";
     }
@@ -80,14 +111,6 @@ const Button: React.FC<ButtonProps> = ({
   const stateStyles = () => {
     if (disabled) {
       return "cursor-not-allowed";
-    }
-    switch (state) {
-      case "pressed":
-        return "transform scale-95";
-      case "hover":
-        return "";
-      default:
-        return "";
     }
   };
 
