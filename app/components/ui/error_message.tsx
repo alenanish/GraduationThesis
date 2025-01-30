d782988ce1737f1512f1175becac0651b476aa4c
Alena Nish <nkm.nnov@gmail.com>
2025-01-30 14:15:26 +0300

    Add error message component

---
+++ app/components/ui/error_message.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ErrorMessageProps {
+  errorMessage: string;
+}
+
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ errorMessage }) => {
+  return (
+    <div
+      className={`flex border-2 bg-red-50 border-red-700 rounded-[32px] py-3 px-2 w-fit min-w-[260px] `}
+    >
+      <label className="w-full text-body-s italic text-center text-base-700">
+        {errorMessage}
+      </label>
+    </div>
+  );
+};
+
+export default ErrorMessage;
