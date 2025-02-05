2855965f1bc6e67c8fb3dc4f855021ee7586a4dd
Alena Nish <nkm.nnov@gmail.com>
2025-02-05 21:29:41 +0300

    Usage of new components

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,10 @@ import ReadOnlyField from "./components/ui/read-only";
 import RadioGroup from "./components/ui/radio-group";
 import CheckboxGroup from "./components/ui/checkbox-group";
 import Switch from "./components/ui/switch";
+import SignInForm from "./components/forms/SignInForm";
+import TopBarButton from "./components/ui/top_bar_button";
+import TopBar from "./components/ui/top_bar";
+import ErrorMessage from "./components/ui/error_message";
 
 export default function Page() {
   const [inputValue, setInputValue] = useState("");
@@ -58,9 +62,37 @@ export default function Page() {
 
   const [isChecked, setIsChecked] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [authError, setAuthError] = useState<string | undefined>(undefined);
+
+  const handleSignIn = async (credentials: {
+    email: string;
+    password?: string;
+  }) => {
+    setIsLoading(true);
+    setAuthError(undefined);
+  };
+
   return (
     <div className="p-4 space-y-4">
-      <h2 className="text-h4 font-bold text-white-900">Buttons</h2>
+      <TopBar />
+
+      <h2 className="text-h4 font-bold text-base-900">Buttons</h2>
+      {/* Secondary Buttons */}
+      <div className="flex space-x-4">
+        <Button type="primary" size="l">
+          Button
+        </Button>
+        <Button type="secondary" size="l">
+          Button
+        </Button>
+        <Button type="tertiary" size="l">
+          Button
+        </Button>
+        <Button type="primary" size="l" disabled>
+          Button
+        </Button>
+      </div>
       <div className="flex space-x-4">
         {/* Primary Buttons */}
         <Button type="primary">
@@ -76,40 +108,83 @@ export default function Page() {
       <div className="flex space-x-4">
         {/* Secondary Buttons */}
 
-        <Button type="primary" size="sm">
+        <Button type="primary" size="s">
           <Circle size={12} />
           Button
           <Circle size={12} />
         </Button>
-        <Button type="secondary" size="sm">
+        <Button type="secondary" size="s">
           Button
         </Button>
-        <Button type="tertiary" size="sm">
+        <Button type="tertiary" size="s">
           Button
         </Button>
-        <Button type="primary" size="sm" disabled>
+        <Button type="primary" size="s" disabled>
           Button
         </Button>
       </div>
-      <h2 className="text-h4 font-bold text-white-900">Icon Buttons</h2>
+      <h2 className="text-h4 font-bold text-base-900">Top Bar Buttons</h2>
       <div className="flex space-x-4">
-        {/* Icon Buttons */}
-        <IconButton size="md" icon={<Circle />} />
-        <IconButton type="secondary" size="md" icon={<Circle />} />
-        <IconButton type="tertiary" size="md" icon={<Circle />} />
-        <IconButton size="md" disabled icon={<Circle />} />
-        <IconButton size="sm" icon={<Circle />} />
-        <IconButton type="secondary" size="sm" icon={<Circle />} />
-        <IconButton type="tertiary" size="sm" icon={<Circle />} />
+        <TopBarButton color="prime" size="s">
+          Button
+        </TopBarButton>
+        <TopBarButton color="base" size="s">
+          Button
+        </TopBarButton>
+        <TopBarButton color="prime" size="m">
+          Button
+        </TopBarButton>
+        <TopBarButton color="base" size="m">
+          Button
+        </TopBarButton>
+        <TopBarButton color="base" size="m" disabled>
+          Button
+        </TopBarButton>
       </div>
-      <h2 className="text-h4 font-bold text-white-900">Input</h2>
+      <div className="flex space-x-4">
+        <TopBarButton type="top-bar-menu" color="prime" size="s">
+          Button
+        </TopBarButton>
+        <TopBarButton type="top-bar-menu" color="base" size="s">
+          Button
+        </TopBarButton>
+        <TopBarButton type="top-bar-menu" color="prime" size="m">
+          Button
+        </TopBarButton>
+        <TopBarButton type="top-bar-menu" color="base" size="m">
+          Button
+        </TopBarButton>
+        <TopBarButton type="top-bar-menu" color="base" size="m" disabled>
+          Button
+        </TopBarButton>
+      </div>
+
+      <h2 className="text-h4 font-bold text-base-900  ">Icon Buttons</h2>
+      <div className="flex flex-row gap-3">
+        {/* Icon Buttons */}
+        <IconButton size="m" icon={<Circle size={32} />} />
+        <IconButton type="secondary" size="m" icon={<EyeIcon size={32} />} />
+        <IconButton type="tertiary" size="m" icon={<Circle size={32} />} />
+        <IconButton size="m" disabled icon={<Circle size={32} />} />
+        <IconButton size="s" icon={<Circle size={24} />} />
+        <IconButton type="secondary" size="s" icon={<Circle size={24} />} />
+        <IconButton type="tertiary" size="s" icon={<Circle size={24} />} />
+        <IconButton
+          type="tertiary"
+          size="l"
+          color="grey"
+          icon={<Circle size={88} />}
+        />
+      </div>
+      <h2 className="text-h4 font-bold text-base-900">Input</h2>
       <div className="flex flex-row space-x-10">
         <div className="flex flex-col space-y-6 w-1/3">
           <Input size="S" placeholder="Enter your name" value={inputValue} />
           <Input
             state="active"
             size="S"
-            placeholder="Active input"
+            placeholder="Name"
+            label="Name"
             rightIcon={<Circle size={16} />}
             value={inputValue}
             onChange={handleChange}
@@ -136,7 +211,7 @@ export default function Page() {
           <Input
             state="enabled"
             size="M"
-            id='name'
+            id="name"
             label="Name"
             placeholder="Enter your name"
             value={inputValue}
@@ -181,7 +256,7 @@ export default function Page() {
       <ReadOnlyField label={"Lable"} text="Read-only text field" />
 
       <div>
-        <h2 className="text-h4 font-bold text-white-900">Radio Group</h2>
+        <h2 className="text-h4 font-bold text-base-900">Radio Group</h2>
         <RadioGroup
           options={radioOptions}
           name="myRadioGroup"
@@ -191,7 +266,7 @@ export default function Page() {
         <p>Выбранное значение: {selectedOption}</p>
       </div>
       <div>
-        <h2 className="text-h4 font-bold text-white-900">Checkbox Group</h2>
+        <h2 className="text-h4 font-bold text-base-900">Checkbox Group</h2>
         <CheckboxGroup
           options={options}
           selectedValues={selectedOptions}
@@ -201,7 +276,7 @@ export default function Page() {
           <p>Selected Options: {selectedOptions.join(", ")}</p>
         </div>
       </div>
-      <h2 className="text-h4 font-bold text-white-900">Switch</h2>
+      <h2 className="text-h4 font-bold text-base-900">Switch</h2>
 
       <div className="flex flex-col space-y-6">
         <Switch
@@ -218,6 +293,14 @@ export default function Page() {
         />
         <Switch label="Option" checked={isChecked} onChange={setIsChecked} />
       </div>
+      <SignInForm
+        onSubmit={handleSignIn}
+        isLoading={isLoading}
+        error={authError}
+      />
+      <ErrorMessage 
+        errorMessage="KFSKFAK"
+      />
     </div>
   );
 }
