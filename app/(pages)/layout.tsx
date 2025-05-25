7a0345d17826fbf90d0ca18c31fb1f84011d9e5a
Alena Nish <nkm.nnov@gmail.com>
2025-05-25 21:26:30 +0300

    Refactor RootLayout

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,18 +2,21 @@
 
 import TopBar from "./_components/top_bar";
 import ProtectedRoute from "../services/protected_route";
+import { useAuth } from "../context/auth_context";
+import TopBarForInvestor from "./_components/top_bar_for_investor";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
+  const { user } = useAuth();
 
   return (
     <ProtectedRoute>
-      <TopBar />
-      <div className="mt-[60px] px-4"> {children} </div>
+      {user?.role === "investor" ? <TopBarForInvestor /> : <TopBar />}
+
+      <div className="mt-[52px] px-4 py-2 overflow-y-auto "> {children} </div>
     </ProtectedRoute>
   );
 }
